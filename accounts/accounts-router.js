ac7ca8fb803f19bca45ffccf5a61a07dd731de16
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-21 19:45:49 -0400

    initial scaffolding

---
+++ accounts/accounts-router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('', (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.post('', (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.put('', (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.delete('', (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+module.exports = router;
